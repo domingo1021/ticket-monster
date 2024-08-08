fc36e9c45cc2c649deaba8cef86c77dd2e1d235f
domingo1021 <domingo.backend@gmail.com>
2024-08-08 23:50:15 +0800

    feat: init ticket monster project

---
+++ ticket-service/src/test/java/cart/ticket/ticketservice/TicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cart.ticket.ticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
